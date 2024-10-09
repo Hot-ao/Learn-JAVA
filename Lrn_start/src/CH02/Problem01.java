ec8ff7998a42508611e968f0c6e066e10fbb2386
Hotao <gkxogh02@naver.com>
2024-10-09 23:54:33 +0900

    세 정수 크기 비교

--- Lrn_start/src/CH02/Problem01.java
+++ Lrn_start/src/CH02/Problem01.java
@@ -1,2 +1,18 @@
 package CH02;
 
+import java.util.Scanner;
+
+ public class Problem01 {
+	 public static void main(String[] args) {
+		 Scanner scanner = new Scanner(System.in);
+		 
+		 System.out.println("정수 3개 입력 >> ");
+		 
+		 int x = scanner.nextInt();
+		 int y = scanner.nextInt();
+		 int z = scanner.nextInt();
+		 
+		 System.out.println((x>y)?((x>z)?x:z):(y>z)?y:z);
+		 
+	 }
+ }
