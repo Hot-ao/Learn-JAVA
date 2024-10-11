dac2d3f7b34c9c25bf002f480fbaf33b6ec87f75
Hotao <gkxogh02@naver.com>
2024-10-11 18:46:19 +0900

    백준 문제 풀이

--- Lrn_start/src/Sprout/Main.java
+++ Lrn_start/src/Sprout/Main.java
@@ -4,19 +4,17 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        int score = scanner.nextInt();
-        String grade;
-
-        switch(score/10) {
-        	case 10:
-        	case 9:	grade="A"; break;
-        	case 8:	grade="B"; break;
-        	case 7:	grade="C"; break;
-        	case 6:	grade="D"; break;
-        	default:	grade="F";
-        }
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
         
-        System.out.println(score/10%10);
+        if (x>0) {
+        	if (y>0) System.out.println("1");
+        	else if(y<0) System.out.println("4");
+        }
+        if (x<0) {
+        	if (y>0) System.out.println("2");
+        	else if (y<0) System.out.println("3");
+        }
         
         scanner.close();
     }
