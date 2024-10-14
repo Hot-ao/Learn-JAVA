190fb977e036a1cfcdf46044538565910a92b5f5
Hotao <gkxogh02@naver.com>
2024-10-14 22:48:20 +0900

    for each문

--- Lrn_start/src/CH03/Ex02.java
+++ Lrn_start/src/CH03/Ex02.java
@@ -25,6 +25,22 @@ public class Ex02{
 			sum += num;
 		}
 		System.out.println(sum/IntArray.length);
+	}
+	
+	enum Week {월, 화, 수, 목, 금, 토, 일}
+	public static void ForEachEx() {
+		int [] n= {1,2,3,4,5};
+		String names[] = {"사과","배","바나나","체리","딸기","포도"};
+		int sum=0;
+		for (int k:n) {
+			System.out.print(k + " ");
+			sum += k;
+		}
+		System.out.println("합은 "+sum);
+		for (String s:names) System.out.print(s+" ");
+		System.out.println();
+		for (Week day:Week.values()) System.out.print(day+"요일 ");
+		System.out.println();
 		
 	}
 //배열	
@@ -37,10 +53,14 @@ public class Ex02{
 		 	* 배열 공유
 		 		* int intArray[] = new int[5];	int myArray[] = intArray; <-- myArray와 intArray가 서로 공유함
 		 	* 배열 크기 - Array.length
-
+		 * 배열&반복문 : for - each 문
+		  	for (int i=0;i<n.length;i++){int k=n[i]; sum+=k;}
+		  	for (int k:n) sum+=k;
+		  	같은 구문
 		 */
 		//ArrayAccess();
-		ArrayMean();
+		//ArrayMean();
+		ForEachEx();
 
 	}
 }
--- Lrn_start/src/Sprout/Main.java
+++ Lrn_start/src/Sprout/Main.java
@@ -6,12 +6,6 @@ public class Main{
     public static void main(String[] args){
     	Scanner sc = new Scanner(System.in);
     	int num = sc.nextInt();
-    	int total=1;
-    	if (num == 0)
-    		total = 1;
-    	else
-    		for (int i=1;i<num+1;i++) total*=i;
-    	System.out.println(total);
-
+    	for (int i=1;i<=num;i++) System.out.println(i);
     }	
 }
