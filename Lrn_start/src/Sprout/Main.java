72dffd7f0e075b03b5396070b48b9c5f5b6a2508
Hotao <gkxogh02@naver.com>
2024-10-15 12:18:02 +0900

    백준 - 새싹 문제 풀이

--- Lrn_start/src/Sprout/Main.java
+++ Lrn_start/src/Sprout/Main.java
@@ -2,10 +2,14 @@ package Sprout;
 
 
 import java.util.Scanner;
-public class Main{
-    public static void main(String[] args){
-    	Scanner sc = new Scanner(System.in);
-    	int num = sc.nextInt();
-    	for (int i=1;i<=num;i++) System.out.println(i);
-    }	
-}
+	public class Main{
+	    public static void main(String[] args){
+	    	Scanner sc = new Scanner(System.in);
+	    	 int T = sc.nextInt();
+	    	 for (int i=0;i<T;i++) {
+	    		 int a = sc.nextInt();
+	    		 int b = sc.nextInt();
+	    		 System.out.println(a+b);
+	    	 }
+	    }	
+	}
