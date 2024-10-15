8f431f75045c0a437e70149821a861c82ea643f0
Hotao <gkxogh02@naver.com>
2024-10-16 01:24:08 +0900

    백준 x보다 작은 수(풀이중)

--- Lrn_start/src/Sprout/Main.java
+++ Lrn_start/src/Sprout/Main.java
@@ -5,11 +5,12 @@ import java.util.Scanner;
 	public class Main{
 	    public static void main(String[] args){
 	    	Scanner sc = new Scanner(System.in);
-	    	 int T = sc.nextInt();
-	    	 for (int i=0;i<T;i++) {
-	    		 int a = sc.nextInt();
-	    		 int b = sc.nextInt();
-	    		 System.out.println(a+b);
-	    	 }
+	    	int N = sc.nextInt();
+	    	int X = sc.nextInt();
+	    	int A[] = new int [N];
+	    	for (int i=0;i<A.length;i++) {
+	    		A[i] = sc.nextInt();
+	    	}
+	    	
 	    }	
 	}
