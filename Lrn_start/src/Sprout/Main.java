2df5d07d54797740b82da5c3cc4f60f97cd58a47
Hotao <gkxogh02@naver.com>
2024-10-18 22:34:54 +0900

    문제 풀이

--- Lrn_start/src/Sprout/Main.java
+++ Lrn_start/src/Sprout/Main.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 	public class Main{
 	public static void main(String[] args){
 	Scanner sc = new Scanner(System.in);
-	int a = sc.nextInt();
-	    	int b = sc.nextInt();
-	    	System.out.println(a+b);
-	    	System.out.println(a-b);
-	    	System.out.println(a*b);
-	    	System.out.println(a/b);
-	    	System.out.println(a%b);
-	    	}
-	    	
-	    }	
+	int N = sc.nextInt();
+	int X = sc.nextInt();
+	int IntArray[] = new int [N];
+	for (int i=0;i<N;i++) {
+		IntArray[i] = sc.nextInt();
+	}
+	for (int i=0;i<N;i++) {
+		if(IntArray[i]<X) System.out.println(IntArray[i]+" ");
+	}
+	}	
+}
