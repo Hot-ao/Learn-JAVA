fc6d5c69b6701ff661aa31a36f053521fcb9e23b
Hotao <gkxogh02@naver.com>
2024-10-20 00:50:32 +0900

    백준 문제 풀이

--- Lrn_start/src/Sprout/Main.java
+++ Lrn_start/src/Sprout/Main.java
@@ -4,15 +4,31 @@ package Sprout;
 import java.util.Scanner;
 	public class Main{
 	public static void main(String[] args){
-	Scanner sc = new Scanner(System.in);
-	int N = sc.nextInt();
-	int X = sc.nextInt();
-	int IntArray[] = new int [N];
-	for (int i=0;i<N;i++) {
-		IntArray[i] = sc.nextInt();
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			double a = sc.nextDouble();
+			double b = sc.nextDouble();
+			double c = sc.nextDouble();
+			double temp;
+			if (a==0&&b==0&&c==0) break;
+			if (a<b) {
+				temp=b;
+				b=a;
+				a=temp;
+			}
+			if (a<c) {
+				temp=a;
+				a=c;
+				c=temp;
+			}
+			if (b<c) {
+				temp=b;
+				b=c;
+				c=temp;
+			}
+			if(a*a==(b*b+c*c)) System.out.println("right");
+			
+			else System.out.println("wrong");
+		}
 	}
-	for (int i=0;i<N;i++) {
-		if(IntArray[i]<X) System.out.println(IntArray[i]+" ");
-	}
-	}	
 }
