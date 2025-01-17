b347492021ba294e6aabc9eb4bf23dd494a1c2f9
gkxog <gkxog@HOTAO_GBP>
2025-01-17 12:53:09 +0900

    Math.random() 식

--- HGJ/src/ch04/step01.java
+++ HGJ/src/ch04/step01.java
@@ -4,7 +4,11 @@ public class step01 {
 
 	public static void main(String[] args) {
 		int num = (int) (Math.random()*100%6+1);
-		System.out.printf("%d번이 나왔습니다.",num);
+		int num_ran = (int)(Math.random()*6) + 1;
+		//Math.random() : 0.0~1.0 중 더블 타입의 난수 하나 리턴
+		//(int) (Math.random()*n) + start
+		//위 식에서 수는 start ~ start+1 사이의 정수가 된다.
+		System.out.printf("%d번이 나왔습니다.",num_ran);
 	}
 
 }
